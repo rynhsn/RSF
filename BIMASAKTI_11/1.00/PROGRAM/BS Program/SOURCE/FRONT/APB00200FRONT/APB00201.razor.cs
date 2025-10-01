1caa87791fc48052daab900d9bbd3059ef46d801
Riyan Husen <riyan.husen@gmail.com>
2025-10-01 16:36:21 +0700

    APB00200

---
+++ BIMASAKTI_11/1.00/PROGRAM/BS Program/SOURCE/FRONT/APB00200FRONT/APB00201.razor.cs
@@ -0,0 +1,87 @@
+﻿using APB00200COMMON.DTO_s;
+using APB00200MODEL.View_Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using R_BlazorFrontEnd;
+using R_BlazorFrontEnd.Controls;
+using R_BlazorFrontEnd.Controls.DataControls;
+using R_BlazorFrontEnd.Controls.Enums;
+using R_BlazorFrontEnd.Controls.Events;
+using R_BlazorFrontEnd.Exceptions;
+using R_BlazorFrontEnd.Helpers;
+using R_BlazorFrontEnd.Interfaces;
+using APB00200FrontResources;
+
+
+namespace APB00200FRONT
+{
+    public partial class APB00201 : R_Page
+    {
+        //variables
+        private APB00200ViewModel _viewModel = new();
+        private R_ConductorGrid _conAPProcessError;
+        private R_Grid<ErrorCloseAPProcessDTO> _gridAPProcessError;
+        [Inject] R_ILocalizer<Resources_Dummy_Class> _localizer { get; set; }
+        [Inject] IJSRuntime _jsRuntime { get; set; }
+        [Inject] R_IExcel _excel { get; set; }
+        private R_eFileSelectAccept[] _accepts = { R_eFileSelectAccept.Excel };
+
+        //methods
+        protected override async Task R_Init_From_Master(object poParameter)
+        {
+            R_Exception loEx = new R_Exception();
+            try
+            {
+                _viewModel.ClosePeriod = R_FrontUtility.ConvertObjectToObject<ClosePeriodDTO>(poParameter);
+                await _gridAPProcessError.R_RefreshGrid(null);
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+
+            loEx.ThrowExceptionIfErrors();
+        }
+        private async Task GridAPCloseError_ServiceGetListRecord(R_ServiceGetListRecordEventArgs eventArgs)
+        {
+            var loEx = new R_Exception();
+            try
+            {
+                await _viewModel.GetListErrorProcessCloseAPPrdAsync();
+                eventArgs.ListEntityResult = _viewModel.ListErrorProcess;
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+            loEx.ThrowExceptionIfErrors();
+        }
+        private async Task SaveToExcel_OnclickAsync()
+        {
+            R_Exception loEx = new R_Exception();
+            try
+            {
+                List<ErrorCloseAPProcessExcelDTO> loExcelList = new(_viewModel.ListErrorProcess.Select(e => new ErrorCloseAPProcessExcelDTO
+                {
+                    No = e.ISEQ_NO,
+                    Description = e.CDESCRIPTION,
+                    Solution = e.CSOLUTION_DESCR
+                }).ToList());
+
+                var loDataTable = R_FrontUtility.R_ConvertTo(loExcelList);
+                loDataTable.TableName = "ErrorList";
+
+                //export to excel
+                var loByteFile = _excel.R_WriteToExcel(loDataTable);
+                var saveFileName = $"CLOSE_AP_CurrentPeriod_ERROR_LIST.xlsx";
+                await _jsRuntime.downloadFileFromStreamHandler(saveFileName, loByteFile);
+                await Close(true, null);
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+            loEx.ThrowExceptionIfErrors();
+        }
+    }
+}
---
+++ BIMASAKTI_11/1.00/PROGRAM/BS Program/SOURCE/FRONT/APB00200MODEL/APB00200Model.cs
@@ -0,0 +1,109 @@
+﻿using APB00200COMMON;
+using APB00200COMMON.DTO_s;
+using APB00200COMMON.DTO_s.Helper;
+using R_APIClient;
+using R_BlazorFrontEnd.Exceptions;
+using R_BusinessObjectFront;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APB00200MODEL
+{
+    public class APB00200Model : R_BusinessObjectServiceClientBase<ClosePeriodDTO>, IAPB00200
+    {
+        //variable & constructors
+        private const string DEFAULT_HTTP = "R_DefaultServiceUrlAP";
+        private const string DEFAULT_ENDPOINT = "api/APB00200";
+        private const string DEFAULT_MODULE = "AP";
+        public APB00200Model(
+            string pcHttpClientName = APB00200Model.DEFAULT_HTTP,
+            string pcRequestServiceEndPoint = DEFAULT_ENDPOINT,
+            bool plSendWithContext = true,
+            bool plSendWithToken = true) :
+            base(pcHttpClientName, pcRequestServiceEndPoint, DEFAULT_MODULE, plSendWithContext, plSendWithToken)
+        {
+        }
+
+        //real methods
+        public async Task<RecordResultAPI<ClosePeriodDTO>> GetClosePeriodAsync(ClosePeriodParam poParam)
+        {
+            var loEx = new R_Exception();
+            RecordResultAPI<ClosePeriodDTO> loResult = new RecordResultAPI<ClosePeriodDTO>();
+            try
+            {
+                R_HTTPClientWrapper.httpClientName = _HttpClientName;
+                loResult = await R_HTTPClientWrapper.R_APIRequestObject<RecordResultAPI<ClosePeriodDTO>, ClosePeriodParam>(
+                    _RequestServiceEndPoint,
+                    nameof(IAPB00200.GetRecord_ClosePeriod),
+                    poParam,
+                    DEFAULT_MODULE,
+                    _SendWithContext,
+                    _SendWithToken);
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+            loEx.ThrowExceptionIfErrors();
+            return loResult;
+        }
+        public async Task<List<ErrorCloseAPProcessDTO>> GetList_ErrorProcessCloseAPPeriodAsync(CloseAPProcessParam poParam)
+        {
+            var loEx = new R_Exception();
+            List<ErrorCloseAPProcessDTO> loResult = null;
+            try
+            {
+                R_HTTPClientWrapper.httpClientName = _HttpClientName;
+                loResult = await R_HTTPClientWrapper.R_APIRequestStreamingObject<ErrorCloseAPProcessDTO, CloseAPProcessParam>(
+                    _RequestServiceEndPoint,
+                    nameof(IAPB00200.GetList_ErrorProcessCloseAPPeriod),
+                    poParam,
+                    DEFAULT_MODULE, _SendWithContext,
+                    _SendWithToken);
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+            loEx.ThrowExceptionIfErrors();
+            return loResult;
+        }
+        public async Task<RecordResultAPI<CloseAPProcessResultDTO>> ProcessCloseAPPeriodAsync(CloseAPProcessParam poParam)
+        {
+            var loEx = new R_Exception();
+            RecordResultAPI<CloseAPProcessResultDTO> loResult = new RecordResultAPI<CloseAPProcessResultDTO>();
+            try
+            {
+                R_HTTPClientWrapper.httpClientName = _HttpClientName;
+                loResult = await R_HTTPClientWrapper.R_APIRequestObject<RecordResultAPI<CloseAPProcessResultDTO>, CloseAPProcessParam>(
+                    _RequestServiceEndPoint,
+                    nameof(IAPB00200.ProcessCloseAPPeriod),
+                    poParam,
+                    DEFAULT_MODULE,
+                    _SendWithContext,
+                    _SendWithToken);
+            }
+            catch (Exception ex)
+            {
+                loEx.Add(ex);
+            }
+            loEx.ThrowExceptionIfErrors();
+            return loResult;
+        }
+
+        //implement only
+        public RecordResultAPI<ClosePeriodDTO> GetRecord_ClosePeriod(ClosePeriodParam poParam)
+        {
+            throw new NotImplementedException();
+        }
+        public RecordResultAPI<CloseAPProcessResultDTO> ProcessCloseAPPeriod(CloseAPProcessParam poParam)
+        {
+            throw new NotImplementedException();
+        }
+        public IAsyncEnumerable<ErrorCloseAPProcessDTO> GetList_ErrorProcessCloseAPPeriod(CloseAPProcessParam poParam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
