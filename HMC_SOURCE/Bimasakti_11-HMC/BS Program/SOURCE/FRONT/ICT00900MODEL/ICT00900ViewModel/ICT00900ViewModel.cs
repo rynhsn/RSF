e17a01f623ca7b720adb991925d927eee101eb7e
Muh Fadel Marzuki <fadel.marzuki@realta.net>
2025-10-28 15:14:41 +0700

    Final Fix Validation ICT00900

--- HMC_SOURCE/Bimasakti_11-HMC/BS Program/SOURCE/FRONT/ICT00900MODEL/ICT00900ViewModel/ICT00900ViewModel.cs
+++ HMC_SOURCE/Bimasakti_11-HMC/BS Program/SOURCE/FRONT/ICT00900MODEL/ICT00900ViewModel/ICT00900ViewModel.cs
@@ -354,6 +354,11 @@ namespace ICT00900MODEL.ICT00900ViewModel
                         var loErr = R_FrontUtility.R_GetError(typeof(Resources_ICT00900_Class), "ValidationAdjustmentValue");
                         loEx.Add(loErr);
                     }
+                    else if (poEntity.CADJUST_METHOD != "A" && poEntity.NADJUST_AMOUNT < 0)
+                    {
+                        var loErr = R_FrontUtility.R_GetError(typeof(Resources_ICT00900_Class), "ValidationAdjustmentValueMin");
+                        loEx.Add(loErr);
+                    }
                 }
 
                 if (string.IsNullOrWhiteSpace(poEntity.CCURRENCY_CODE))
