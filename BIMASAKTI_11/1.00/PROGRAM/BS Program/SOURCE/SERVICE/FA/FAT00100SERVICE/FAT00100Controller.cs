9dfe3b0bc4d85015bbb6f2ce8fb8e4b7e8215aea
zulfiandri <zulfiandri09@outlook.com>
2026-02-13 17:52:57 +0700

    fat00100, fat00800, PMT03500

--- BIMASAKTI_11/1.00/PROGRAM/BS Program/SOURCE/SERVICE/FA/FAT00100SERVICE/FAT00100Controller.cs
+++ BIMASAKTI_11/1.00/PROGRAM/BS Program/SOURCE/SERVICE/FA/FAT00100SERVICE/FAT00100Controller.cs
@@ -404,7 +404,8 @@ namespace FAT00100Service
                 var loCls = new FAT00100Cls();
 
                 // Set global variables from R_BackGlobalVar
-                poParameter.CCOMPANY_ID = R_BackGlobalVar.COMPANY_ID;
+                //poParameter.CCOMPANY_ID = R_BackGlobalVar.COMPANY_ID;
+                poParameter.CCOMPANY_ID = "bsu";
                 poParameter.CLANG_ID = R_BackGlobalVar.CULTURE;
                 poParameter.CUSER_ID = R_BackGlobalVar.USER_ID;
 
